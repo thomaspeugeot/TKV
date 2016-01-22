fdc03f6efc9e1f39e933229f24f615fd765d1f21
Peugeot <thomas.peugeot@moss.fr>
2016-01-22 17:23:53 +0100

    Clarification

--- quadtree/quadtree.go
+++ quadtree/quadtree.go
@@ -1,23 +1,36 @@
-// compact implementation of a static 2D quadtree
+// Compact implementation of a static 2D quadtree
 // 
-// goal is to support the Barnes Hut implementation for spreading bodies on a 2D rectangle.
+// 1st Goal is to support the Barnes Hut implementation for spreading bodies on a 2D square.
+// 
+// 2nd Goal is to support more than 1 million bodies
+// there are constraints:
+//
+// - X,Y coordinates are float64 between 0 & 1
+//
+// - quadtree architecture is static
+//
+// - depth is limited to 8 (256 * 256 cells at the level 8)
+//
+// to see the doc
+//
+// 		godoc -goroot=$GOPATH -http=:8080
 package quadtree
 
-// the coordinate of a node
-// this is coded on a uint32 for speed sake
-// the first byte encode the depth of the node (256 levels seems OK)
-//           0 is the root node
-//			 1 is the level 1 node
-//           ...
-// second to fourth byte encode the coordinate of the node by block of 2 bits by level
-// 00 is for north west
-// 01 is for north east
-// 10 is for south west
-// 11 is for south east
-// for instance 
-// 0x00 0x00 0x00 0x00 is the root node
-// 0x01 0x00 0x00 0x00 is the level 1 node on north west
+// 
+// Coordinate system of a node
+// 
+// Coordinates of a node are coded as follow
 //
+// 	1st byte : level (root = 0, max depth = 7) 
+// 	2nd byte : X coordinate 
+// 	3nd byte : Y coordinate : coded on 
+//	4th byte : 0x00 (unused) 
+//
+// the X coordinate code depends on the level
+//	level 0: there is no coordinate
+//	level 1: west if 0, east is 128 (0x80)
+//	level 2: quarters coordinates are 0, 64 (0x40), 128 (0x80), 192 (0x84)
+//	...
 type Coord uint32
 
 // get node level from node coord c
@@ -27,8 +40,31 @@ func Level(c Coord) int {
 	return result
 }
 
-// initiate a node coordinate from level
-// and set of coordinates
-func set( c *Coord, level int, coords []int) {
+// check encoding of c
+func checkIntegrity( c Coord) bool {
+	
+	// check byte 4 is null
+	if res := 0x000000FF & c; res != 0x00 {
+		return false
+	}
+	
+	return true
+}
+
+// suite of integer between 0 & 3 denoting position of the 
+// node within the node above level.
+type PosSuite []int
+
+// initiate a node coordinate from level and coords
+// coords is an array of int with length level
+func set( c *Coord, level int, coords PosSuite) bool {
+	result := true
+	
+	if len(coords) != level { 
+		result = false
+	}
+	
+	
+	return result
 }
 
--- quadtree/quadtree_test.go
+++ quadtree/quadtree_test.go
@@ -24,3 +24,44 @@ func TestLevel(t *testing.T) {
 		}	
 	}
 }
+
+func TestCheckIntegrity( t *testing.T) {
+	
+	cases := []struct {
+		in Coord
+		want bool
+	}{
+		{ 0x00, true},
+		{ 0x000000FF, false},
+		{ 0x000A0001, false},
+		{ 0x000A0000, true},
+	}
+	for _, c := range cases {
+		got := checkIntegrity(c.in)
+		if( got != c.want) {
+			t.Errorf("checkIntegrity(%b) == %t, want %t", c.in, got, c.want)
+		}	
+	}
+}
+
+func TestSet(t * testing.T) {
+
+	cases := []struct {
+		inCoord Coord
+		inLevel int
+		inCoords PosSuite
+		want bool
+	}{
+		{	0, 			2, 			PosSuite{1, 2}, 			true}, // level is OK
+		{	0, 			2, 			PosSuite{1, 4}, 			false}, // one pos is above 3
+		{	0, 			2, 			PosSuite{1, -1}, 			false}, // one pos is below 0
+		{	0, 			1, 			PosSuite{1, 2}, 			false}, // level is not good
+ 	}
+		
+	for _, c := range cases {
+		got := set( &c.inCoord, c.inLevel, c.inCoords)
+		if( got != c.want) {
+			t.Errorf("Set(%b, %d, %q) == %t, want %t", c.inCoord, c.inLevel, c.inCoords, got, c.want)
+		}	
+	}
+}
