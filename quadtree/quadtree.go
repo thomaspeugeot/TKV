11782a42a1db8834d984e5b06a325c6907bb994f
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2016-01-25 08:45:35 +0100

    added function

--- quadtree/quadtree.go
+++ quadtree/quadtree.go
@@ -19,6 +19,10 @@
 // 		godoc -goroot=$GOPATH -http=:8080
 package quadtree
 
+import (
+	"fmt"
+)
+
 // 
 // Coordinate system of a node
 //
@@ -50,14 +54,59 @@ package quadtree
 type Coord uint32
 
 // node level of a node coord c
-// is between 0 and 8
-func Level(c Coord) int { return int( c >> 16) }
+// is between 0 and 8 and coded on 2nd byte of the Coord c
+func (c Coord) getLevel() int { return int( c >> 16) }
+func (c * Coord) setLevel(level int) { 
+	var pad uint32
+	pad = (uint32(level) << 16) & 0x0000FFFF
+	*c = *c & Coord(pad)
+}
 
-// x coord node coord c
-func x(c Coord) int { return int((c & 0x0000FFFF) >> 8) }
+// x coord
+func (c Coord) getX() int { return int((c & 0x0000FFFF) >> 8) }
+func (c * Coord) setX(x int) { 
+	var pad uint32
+	pad = (uint32(x) << 8) & 0x00FFFFFF
+	*c = *c & Coord(pad)
+	// if !checkIntegrity( *c) { panic("set X failed")}
+}
 
-// y coord node coord c
-func y(c Coord) int { return int( c & 0x000000FF) }
+// y coord
+func (c Coord) getY() int { return int( c & 0x000000FF) }
+func (c Coord) setY(y int) { 
+	var pad uint32
+	pad = uint32(y) & 0x00FFFFFF
+	c = c & Coord(pad)
+}
+
+// a body is a position & a mass
+type Body struct {
+	X float64
+	Y float64
+	M float64
+}
+
+// a node is a body (
+type Node struct {
+	Bodies []Body // bodies of the node (only at level 8, above, the list is empty)
+	Body // barycenter with mass of all the bodies of the node
+}
+
+// a Quadtree store Nodes. It is a an array with direct access to the Nodes with the Nodes coordinate
+// see Coord
+type Quadtree [8*256*256]Node
+
+// compute Node coordinates at level 8 of a body
+func ( b Body) getCoord8() Coord {
+	var c Coord
+	
+	(&c).setLevel( 8)
+	fmt.Printf( "After SetLevel c %8x\n", c)
+	c.setX( int(b.X * 256.0) )
+	c.setY( int(b.Y * 256.0) )
+	
+	return c
+}
 
 // check encoding of c
 func checkIntegrity( c Coord) bool {
@@ -68,17 +117,18 @@ func checkIntegrity( c Coord) bool {
 	}
 	
 	// check level is below or equal to 8
-	if Level( c) > 8 {
+	if c.getLevel() > 8 {
 		return false
 	}
 	
 	// check x coord is encoded acoording to the level
-	if (0xFF >> uint( 8 - Level(c))) & x(c) != 0x00 {
+	if (false) { fmt.Printf( "y (0xFF >> uint( setLevel(%d))) %08b\n", c.getLevel(), 0xFF >> uint( c.getLevel())) }
+	if (0xFF >> uint( c.getLevel())) & c.getX() != 0x00 {
 		return false
 	}
 
 	// check y coord
-	if (0xFF >> uint( 8 - Level(c))) & y(c) != 0x00 {
+	if (0xFF >> uint( c.getLevel())) & c.getY() != 0x00 {
 		return false
 	}
 	
--- quadtree/quadtree_test.go
+++ quadtree/quadtree_test.go
@@ -2,6 +2,7 @@ package quadtree
 
 import (
 	"testing"
+	"math/rand"
 )
 
 
@@ -18,15 +19,17 @@ func TestLevel(t *testing.T) {
 	}
 		
 	for _, c := range cases {
-		got := Level(c.in)
+		got := c.in.getLevel()
 		if( got != c.want) {
-			t.Errorf("Level(%32b) == %d, want %d", c.in, got, c.want)
-			t.Errorf("Level(|%8b|%8b|%8b|%8b|) == %d, want %d", 
+			t.Errorf("getLevel(%32b) == %d, want %d", c.in, got, c.want)
+			t.Errorf("getLevel(|%8b|%8b|%8b|%8b|) == %d, want %d", 
 					0x000000FF & (c.in >> 24), 0x000000FF & (c.in >> 16), 0x000000FF & (c.in >> 8), 0x000000FF & c.in, 
 					got, c.want)
 		}	
 	}
 }
+
+
 
 func TestIntegrity( t *testing.T) {
 	
@@ -35,11 +38,14 @@ func TestIntegrity( t *testing.T) {
 		want bool
 	}{
 		{ 0x00, true},
-		{ 0x000000FF, false},
-		{ 0x000a0001, false},
-		{ 0x00070001, true},
+		{ 0x000000FF, false}, // at level 0, no bits are allowed for x or y
+		{ 0x000a0001, false}, // level a is above 8
+		{ 0x00070010, true}, // level 7 is OK
+		{ 0x00070001, false}, // the last bit shall be 0
+		{ 0x00070101, false}, // the last bit of x shall be 0
+		{ 0x00080001, true}, // the last bit can be 1
 		{ 0x000A0000, false},
-		{ 0x0A0A0000, false},
+		{ 0x0A0A0000, false}, // byt0 shall be null
 	}
 	for rank, c := range cases {
 		got := checkIntegrity(c.in)
@@ -48,14 +54,69 @@ func TestIntegrity( t *testing.T) {
 			t.Errorf("case %d - checkIntegrity of |%8b|%8b|%8b|%8b|, %8x, level %d == %t, want %t", 
 					rank,
 					0x000000FF & (c.in >> 24), 0x000000FF & (c.in >> 16), 0x000000FF & (c.in >> 8), 0x000000FF & c.in, 
-					c.in, Level( c.in),
+					c.in, c.in.getLevel(),
 					got, c.want)
 		}	
 	}
 }
 
-func TestSet(t * testing.T) {
+// init a quadtree with random position
+//
+func initQuadtree( qa * Quadtree, nbBodies int) {
+	
+	var q Quadtree
+	b := make([]Body, nbBodies)
+	
+	// init bodies
+	for i := 0; i < nbBodies; i++ {
+		b[i].X = rand.Float64()
+		b[i].Y = rand.Float64()
+		b[i].M = rand.Float64()
+	}
+	
+	//
+	qa = &q
+	
+}
 
+func BenchmarkInitQuadtree(b * testing.B) {
+	for i := 0; i<b.N;i++ {
+		var q Quadtree
+		initQuadtree( &q , 1000000)
+	}
+
+}
+
+func BenchmarkSetLevel(b * testing.B) {
+	for i := 0; i<b.N;i++ {		var c Coord;	c.setLevel(6) }
+}
+
+func BenchmarkSetX(b * testing.B) {
+	for i := 0; i<b.N;i++ {		var c Coord;	c.setX(6) }
+}
+
+func BenchmarkSetY(b * testing.B) {
+	for i := 0; i<b.N;i++ {		var c Coord;	c.setY(6) }
+}
+
+func TestGetCoord8(t * testing.T) {
+	cases := []struct {
+		in Body
+		want Coord
+	}{
+		{ Body{0.0, 0.0, 0.0}, 0x00080000 }, 
+	}
+	for _, c := range cases {
+		got := c.in.getCoord8()
+		if( got != c.want) {
+			t.Errorf("getCoord8(%#v) == |%8b|%8b|%8b|%8b|, %8x, want %8x", c.in, 
+			0x000000FF & (got >> 24), 0x000000FF & (got >> 16), 
+			0x000000FF & (got >> 8), 0x000000FF & got, got, c.want) 
+		}
+	}
+}
+
+func TesSet(t * testing.T) {
 	cases := []struct {
 		inCoord Coord
 		inLevel int
