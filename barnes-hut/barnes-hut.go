2d7d472942600b0b9e79f875ac3a915350c25a8f
Peugeot <thomas.peugeot@moss.fr>
2016-02-02 18:52:08 +0100

    getting to know os package

--- barnes-hut/barnes-hut.go
+++ barnes-hut/barnes-hut.go
@@ -11,6 +11,11 @@ package barnes_hut
 
 import (
 	"github.com/thomaspeugeot/tkv/quadtree"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"fmt"
 )
 
 //	Bodies's X,Y position coordinates are float64 between 0 & 1
@@ -38,21 +43,27 @@ type Body struct {
 	Acc
 }
 
+var palette = []color.Color{color.White, color.Black}
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
 // a simulation run
 type Run struct {
-	bodies []quadtree.Body // bodies
+	bodies * []quadtree.Body // bodies
 	q quadtree.Quadtree // the supporting quadtree
 }
 
 func (r * Run) Init( bodies * ([]quadtree.Body)) {
-	r.bodies = *bodies
+	r.bodies = bodies
 	r.q.SetupNodesLinks()
 }
 
-func (r * Run) oneStep( bodies * ([]quadtree.Body)) {
+func (r * Run) oneStep() {
 
 	// compute the quadtree from the bodies
-	r.q.UpdateNodesListsAndCOM( bodies)
+	r.q.UpdateNodesListsAndCOM( r.bodies)
 	
 	// compute repulsive forces & acceleration
 	
@@ -60,4 +71,48 @@ func (r * Run) oneStep( bodies * ([]quadtree.Body)) {
 	
 	// compute new position
 	
+}
+
+func (r * Run) ComputeRepulsiveForce() {
+
+	// parse all bodies
+	for idx, _ := range (*r.bodies) {
+		
+		body := (*r.bodies)[idx]
+		
+		// parse all repulsions
+		for idx2, _ := range (*r.bodies) {
+			body2 := (*r.bodies)[idx2]
+			
+		}
+	}
+}
+
+
+func (r * Run) outputGif(out io.Writer) {
+	const (
+		size    = 500   // image canvas covers [-size..+size]
+		nframes = 1    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	anim := gif.GIF{LoopCount: nframes}
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		for idx, _ := range (*r.bodies) {
+		
+			body := (*r.bodies)[idx]
+		
+			if false { fmt.Printf("Encoding body %d %f %f\n", idx, body.X, body.Y) }
+		
+			img.SetColorIndex(
+				size+int(body.X*size+0.5), 
+				size+int(body.Y*size+0.5),
+				blackIndex)
+		}
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
 }
--- barnes-hut/barnes-hut_test.go
+++ barnes-hut/barnes-hut_test.go
@@ -1,22 +1,69 @@
 package barnes_hut
 
 import (
+	"os"
 	"github.com/thomaspeugeot/tkv/quadtree"
 	"testing"
 	"fmt"
+	"math"
+	"math/rand"
 )
 
 // init 
+func TestOutputGif(t *testing.T) {
+
+	bodies := make([]quadtree.Body, 100000)
+	spreadOnCircle( & bodies)
+	
+	var r Run
+	r.Init( & bodies)
+	
+	var output *os.File
+	output, _ = os.Create("essai.gif")
+	
+	r.outputGif( output)
+	// visual verification
+}
+
+func Test20Steps(t *testing.T) {
+	
+}
 
 // benchmark init
 func BenchmarkInitRun1000000(b * testing.B) {
 	
 	bodies := make([]quadtree.Body, 1000000)
 
-	fmt.Printf("\n%#v", bodies[0])
+	if false { fmt.Printf("\n%#v", bodies[0]) }
+	
+	spreadOnCircle( & bodies)
 	
 	var r Run
 	for i := 0; i<b.N;i++ {
 		r.Init( & bodies)
+	}
+}
+
+func spreadOnCircle(bodies * []quadtree.Body) {
+	for idx, _ := range *bodies {
+		
+		body := &((*bodies)[idx])
+		
+		radius := 0.6 * rand.Float64()
+		angle := 2.0 * math.Pi * rand.Float64()
+		
+		if idx%2 == 0 {
+			body.X += 0.4
+			body.Y += 0.7
+			radius *= 0.5
+		} else {
+			body.X -= 0.2
+			body.Y -= 0.1
+			radius *= 1.3
+		}
+		
+		body.M =0.1000000
+		body.X += math.Cos( angle) * radius
+		body.Y += math.Sin( angle) * radius
 	}
 }
