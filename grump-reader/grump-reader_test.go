33fa997215642cdb42b41fb7ca98a83ea71311fd
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2016-04-24 12:26:14 +0200

    avancement sur la génération de densité

--- grump-reader/grump-reader_test.go
+++ grump-reader/grump-reader_test.go
@@ -4,12 +4,15 @@ import (
 	"image"
 	"image/color"
 	"image/gif"
-
+	"math"
 	"testing"
 	"bufio"
 	"fmt"
 	"os"
 	"log"
+	"github.com/thomaspeugeot/tkv/country"
+	// "github.com/thomaspeugeot/tkv/barnes-hut"
+	"github.com/thomaspeugeot/tkv/quadtree"
 )
 
 var palette = []color.Color{
@@ -43,6 +46,7 @@ const (
 
 func BenchmarkReadGrumpNationalities(b * testing.B ) {
 
+	// open grump file
 	var grumpNtlFile *os.File
 	var err error
 	grumpNtlFile, err = os.Open("/Users/thomaspeugeot/the-mapping-data/gl_grumpv1_ntlbndid_ascii_30/gluntlbnds.asc")
@@ -50,12 +54,13 @@ func BenchmarkReadGrumpNationalities(b * testing.B ) {
 		log.Fatal(err)
 	}	
 
+	// open output map
 	popMapFile, _ := os.Create("popMapFile.gif")
 	anim := gif.GIF{LoopCount: 1}
 
 	// for debug sake, introduce which ratio of the 
 	// input date is processed
-	ratioOfProcessedLines := 0.4
+	ratioOfProcessedLines := 0.99
 	maxNbLines := int( ratioOfProcessedLines * float64(17100))
 	maxNbCols := 43200
 
@@ -67,7 +72,55 @@ func BenchmarkReadGrumpNationalities(b * testing.B ) {
 	rect := image.Rect(0, 0, maxImageX, maxImageY)
 	img := image.NewPaletted(rect, palette)
 
+	// prepare borders of country
+	type countryBoundary struct {
+		topLat float64
+		bottomLat float64
+		westLng float64
+		eastLng float64
+	}
 
+	// prepare the density file
+	var bodies []quadtree.Body
+	quadtree.InitBodiesUniform( &bodies, 200000)
+
+	// prepare one file per country (for 1 to 229)
+	countryGifFiles := make(map[int](* os.File))
+	countryRectangle := make(map[int] image.Rectangle)
+	countryPalette := make(map[int](* image.Paletted))
+	countryAnim  := make(map[int]gif.GIF)
+	countryName := make(map[int]string)
+	countryBoundaries := make(map[int]*countryBoundary)
+	countryFinalBoundaries := make(map[int]*countryBoundary)
+
+	// parse countries and make a match between index and known country
+	for index:= 0; index<=230; index++ {
+		countryCodeGrump := country.CountryCodes[index]
+		countryName[ countryCodeGrump.CodeGrump ] = countryCodeGrump.Name
+				
+		countryBorder := country.CountryBorders[index]
+		countryFinalBoundaries[ countryBorder.Index] = & countryBoundary{ countryBorder.TopLat,
+		countryBorder.BottomLat,
+		countryBorder.EastLng,
+		countryBorder.WestLng }
+		
+	}
+
+	for index:= 0; index<=230; index++ {
+	
+		countryString, _ := countryName[ index]
+
+		popMapCountryFile, _ := os.Create( fmt.Sprintf("popMapFile%03d-%s.gif", index, countryString))
+		countryGifFiles[index] = popMapCountryFile
+		countryRectangle[index] = image.Rect(0, 0, maxImageX, maxImageY)
+		countryPalette[index] = image.NewPaletted(rect, palette)
+		countryAnim[index] = gif.GIF{LoopCount: 1}
+	
+		countryBoundaries[index] = & countryBoundary{ -90.0,+90.0,180.0,-180.0}
+		// prepare the output per country
+	}
+
+	// parse the grump
 	var nbWords int
 	scanner := bufio.NewScanner( grumpNtlFile)
 	scanner.Split(bufio.ScanWords)
@@ -134,17 +187,61 @@ func BenchmarkReadGrumpNationalities(b * testing.B ) {
 			}
 
 			if( value != -9999) {
+				if( nbLines %100 == 0) {
+					fmt.Printf("\rval %03d line %5d lat %2.3f ln %03.3f ", value, nbLines, lineLat, math.Abs(columnLong))
+				}
+
 				img.SetColorIndex(
 					int( float64(nbWords) * displayRatioY), 
 					int( float64(nbLines) * displayRatioY), 
 					pixelIndex)
+				if _, ok := countryPalette[value]; ok {
+					countryPalette[value].SetColorIndex(
+						int( float64(nbWords) * displayRatioY), 
+						int( float64(nbLines) * displayRatioY), 
+						blackIndex)
+				}
+				if boundary, ok := countryBoundaries[value]; ok {
+					if boundary.topLat < lineLat {	boundary.topLat = lineLat }
+					if boundary.bottomLat > lineLat { boundary.bottomLat = lineLat }
+					if boundary.westLng > columnLong { boundary.westLng = columnLong}
+					if boundary.eastLng < columnLong { boundary.eastLng = columnLong}
+				}
 			}
 			// fmt.Println(scanner.Text()) // Println will add back the final '\n'
 		}
 		nbLines++
-		fmt.Printf("\rline %5d lat %2.3f", nbLines, lineLat)
 
 	}
+
+	// indicate the corners of each country
+	for index, boundary := range countryBoundaries {
+
+		nbLinesTop := math.Floor( (topLat - boundary.topLat) / lineLatWidth)
+		nbLinesBottom := math.Floor( (topLat - boundary.bottomLat) / lineLatWidth)
+		nbWordsWest := math.Floor( (boundary.westLng - westLong) / columnLongWdth)
+		nbWordsEast := math.Floor( (boundary.eastLng - westLong) / columnLongWdth)
+
+		countryPalette[index].SetColorIndex(
+						int( float64(nbWordsWest) * displayRatioY), 
+						int( float64(nbLinesTop) * displayRatioY), 
+						redIndex)
+		countryPalette[index].SetColorIndex(
+						int( float64(nbWordsWest) * displayRatioY), 
+						int( float64(nbLinesBottom) * displayRatioY), 
+						redIndex)
+		countryPalette[index].SetColorIndex(
+						int( float64(nbWordsEast) * displayRatioY), 
+						int( float64(nbLinesTop) * displayRatioY), 
+						redIndex)
+		countryPalette[index].SetColorIndex(
+						int( float64(nbWordsEast) * displayRatioY), 
+						int( float64(nbLinesBottom) * displayRatioY), 
+						redIndex)
+		fmt.Printf("{%d,\"%s\",%f,%f,%f,%f},\n", index, countryName[index], boundary.topLat, boundary.bottomLat, boundary.westLng, boundary.eastLng)
+	}
+
+	// print the country
 	for country, n := range countries {
 		fmt.Printf("%d\t%d\n", country, n)
 	}
@@ -162,5 +259,13 @@ func BenchmarkReadGrumpNationalities(b * testing.B ) {
 	gif.EncodeAll(popMapFile, &anim) // NOTE: ignoring encoding errors
 	popMapFile.Close()
 
+	for index:= 0; index<=230; index++ {
+		gifImage := countryAnim[index]
+
+		gifImage.Delay = append( gifImage.Delay, 8)
+		gifImage.Image = append( gifImage.Image, countryPalette[index])
+		gif.EncodeAll(countryGifFiles[index], &gifImage) // NOTE: ignoring encoding errors
+		countryGifFiles[index].Close()
+	}
 }
 
