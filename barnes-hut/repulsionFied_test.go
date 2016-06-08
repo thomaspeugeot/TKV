dcf85f309bf62d4f43dd95b024912491f92bca4c
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2016-06-08 07:24:41 +0200

    scaffolding field computation

--- barnes-hut/repulsionFied_test.go
+++ barnes-hut/repulsionFied_test.go
@@ -1,11 +1,37 @@
 package barnes_hut
 
 import (
+	"github.com/thomaspeugeot/tkv/quadtree"
 	"testing"
 )
 
 func TestRepulsionFieldInit(t *testing.T) {
 
-	f := NewRepulsionField( 100)
+	bodies := make([]quadtree.Body, 10 * 10)
+	SpreadOnCircle( & bodies)
+	var r Run
+	r.Init( & bodies)
+
+	f := NewRepulsionField( 0.3, 0.5, 
+							0.4, 0.6, 
+							4,
+							& (r.q)) // quadtree
 	f.ComputeField()
+
+	cases := make( []struct {
+		i, j int
+		wantX, wantY float64
+	}, 1)
+	
+	cases[0].i = 1
+	cases[0].j = 2
+	cases[0].wantX = 0.3375 // 0.3 + 0.125 * (1 + 2*1)
+	cases[0].wantY = 0.5625 // 0.5 + 0.125 * (1 + 2*2)
+
+	for _, c := range cases {
+		gotX, gotY := f.XY( c.i, c.j)
+		if( (gotX != c.wantX) && (gotY != c.wantY)) {
+			t.Errorf("i %d j %d == %f %f, want %f %f", c.i, c.j, gotX, gotY, c.wantX, c.wantY )
+		}	
+	}
 }
--- barnes-hut/repulsionField.go
+++ barnes-hut/repulsionField.go
@@ -1,24 +1,31 @@
 package barnes_hut
 
 import (
-	// "github.com/thomaspeugeot/tkv/quadtree"
+	"github.com/thomaspeugeot/tkv/quadtree"
 )
 
 // a RepulsionField stores the computation
 // of a scalar field with the values of the repulsion field (1/r)
-// on a fixed area
+// on a fixed area, at interpolation points ( GridFieldTicks interpolation points per dimension )
 // this structure is transcient 
 type RepulsionField struct {
 	XMin, YMin, XMax, YMax float64 // coordinate of the rendering area
 	GridFieldTicks int // nb of intervals where the field is computed
 	// q * quadtree.Quadtree // quadtree used to compute the field
 	values [][]float64 // values of the field
+	q * quadtree.Quadtree // the quadtree against which the field is computed
 }
 
-func NewRepulsionField(GridFieldTicks int) * RepulsionField {
+func NewRepulsionField( XMin, YMin, XMax, YMax float64, GridFieldTicks int, q * quadtree.Quadtree) * RepulsionField {
 	Info.Println("NewRepulsionField")
 
 	var f RepulsionField
+	// to be replaced with a proper init of struct to 
+	f.XMin = XMin
+	f.YMin = YMin
+	f.XMax = XMax
+	f.YMax = YMax
+
 	f.GridFieldTicks = GridFieldTicks
 	f.values = make ( [][]float64, GridFieldTicks)
 	for i,_ := range f.values {
@@ -27,14 +34,37 @@ func NewRepulsionField(GridFieldTicks int) * RepulsionField {
 	return &f
 }
 
+// compute x,y coordinates in the square from the i,j coordinate in the field
+func (f * RepulsionField) XY(i, j int) (x,y float64) {
+
+	x = f.XMin + ((float64(i)+0.5) / float64( f.GridFieldTicks)) * (f.XMax - f.XMin)
+	y = f.YMin + ((float64(j)+0.5) / float64( f.GridFieldTicks)) * (f.YMax - f.YMin)
+
+	return x, y
+}
+
+// compute field for all interpolation points
 func (f * RepulsionField) ComputeField() {
 	Info.Println("ComputeField nbTicks ", f.GridFieldTicks, len( f.values))
 
 	for i,vs := range f.values {
 		for j,v := range vs {
-			Trace.Printf("computeField at %d %d, v = %e\n", i, j, v)
+			
+			x, y := f.XY( i, j)
+			
+
+			var rootCoord quadtree.Coord
+			f.ComputeFieldAtInterpolationPointRecursive( x, y, f.q, rootCoord)
+			Info.Printf("computeField at %d %d %e %e, v = %e\n", i, j, x, y, v)
 		}
 	} 
 }
+
+func (f * RepulsionField) ComputeFieldAtInterpolationPointRecursive( x, y float64, q * quadtree.Quadtree, coord quadtree.Coord) float64 {
+
+	return 0.0
+}
+
+
 
 
