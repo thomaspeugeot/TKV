95866cb0a4894fe74ab58b8e45a6d6d29b429c86
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2016-09-19 22:20:21 +0200

    correction of the insert function

--- barnes-hut/neighbour.go
+++ barnes-hut/neighbour.go
@@ -62,6 +62,17 @@ func (dico * NeighbourDico) Insert(index int, body * quadtree.Body, distance flo
 		Trace.Printf( "Insert begin with distance %f", distance)
 	}
 
+	//check if body is already present
+	// if yes, update distance
+	for rank := NbOfNeighboursPerBody-1; rank >=0; rank-- {
+		if (*dico)[index][rank].n == body {
+			if (*dico)[index][rank].Distance > distance {
+				(*dico)[index][rank].Distance = distance
+			}
+			return
+		}
+	}
+
 	// check if body is eligible to the last rank
 	// replace if last rank is nil or if distance is greater 
 	if( (*dico)[index][NbOfNeighboursPerBody-1].n == nil || 
@@ -128,20 +139,32 @@ func (dico * NeighbourDico) ComputeRatioOfNilNeighbours() float64 {
 // the neighbours at the origin
 func (dico * NeighbourDico) ComputeStirring(dicoOrig * NeighbourDico) float64 {
 
+	numberOfNeighbors := 0 // number of neighbours in orig that are not nil 
 	numberOfKeptNeighbors := 0 
 	for idx,_  := range *dico {
-		for n, _ := range (*dico)[idx] {
+		for nOrig, _ := range (*dicoOrig)[idx] {
 
-			body := (*dico)[idx][n].n
-			// parse dico orig to check is the body is present
-			for nOrig , _ := range (*dicoOrig)[idx] {
-				if (*dicoOrig)[idx][nOrig].n == body {
-					numberOfKeptNeighbors++
-					continue
+			bodyOrig := (*dicoOrig)[idx][nOrig].n
+
+			if bodyOrig != nil {
+				numberOfNeighbors++ 
+				
+				// parse dico to check if the orig neighbour's body is present
+				for n , _ := range (*dico)[idx] {
+					found :=0
+					if (*dico)[idx][n].n == bodyOrig {
+						numberOfKeptNeighbors++
+						found++
+						// continue
+					}
+					if found > 1 {
+						Info.Printf("At index %d, Neighbours %d found twice", idx, nOrig)
+					}
 				}
 			}
 		}	
 	}
 
-	return float64(numberOfKeptNeighbors)/float64(len(*dico))
+	Trace.Printf("numberOfNeighbors %d numberOfKeptNeighbors %d", numberOfNeighbors, numberOfKeptNeighbors)
+	return float64(numberOfKeptNeighbors)/float64(numberOfNeighbors)
 }
