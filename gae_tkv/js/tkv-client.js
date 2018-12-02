06969fadd94420480b4f9f1e0327c83984e6b9f8
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2018-12-02 18:29:55 +0100

    simplify initial page

--- gae_tkv/js/tkv-client.js
+++ gae_tkv/js/tkv-client.js
@@ -8,11 +8,6 @@ mapOfMaps.set( "hti", hti);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	maxZoom: 18,
-	attribution: 
-		'A propos 10 000 territories; <a href="https://10ktblog.wordpress.com/a-propos/">10 000</a> contributors, ' +
-		'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	id: 'mapbox.streets'
 }).addTo(fra);
 
@@ -105,9 +100,9 @@ hti.on('click', onMapClick);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	maxZoom: 18,
 	attribution: 
-		'A propos 10 000 territories; <a href="https://10ktblog.wordpress.com/a-propos/">10 000</a> contributors, ' +
-		'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+		'<a href="https://10ktblog.wordpress.com/a-propos/">10 000</a> ' +
+		'<a href="https://www.openstreetmap.org/">OpenStreetMap</a>  ' +
 		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		'<a href="https://www.mapbox.com/">Mapbox</a>',
 	id: 'mapbox.streets'
 }).addTo(hti);
